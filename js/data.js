0385260b201aefe06dfdf1a9d6d5afc2ba5246dc
AtmoclubGames <russti@yandex.ru>
2023-04-03 16:34:11 +0700

    add start game window

--- js/data.js
+++ js/data.js
@@ -15,3 +15,22 @@ for (let index = 0; index < menuItems.length; index++) {
   
     
 }
+const gameGenres=['Экшен','Аркады','Ролевые игры','Шутеры',"Стратегии","Гонки","Симуляторы","Викторины"];
+
+function createGenreMenu() {
+  const genreMenu = document.getElementById('genre-menu');
+
+  gameGenres.forEach(genre => {
+    const li = document.createElement('li');
+    const a = document.createElement('a');
+    a.innerText = genre;
+    a.href = `#${genre.toLowerCase()}`;
+    li.appendChild(a);
+    genreMenu.appendChild(li);
+  });
+}
+
+window.onload = function() {
+  createGenreMenu();
+  // other code
+}
--- js/main.js
+++ js/main.js
@@ -27,3 +27,71 @@ gameCards.forEach(element => {
     // element.innerText=''
     element.style.backgroundImage=`url(/images/gameCards/${gameCardImages[getRandom(0,gameCardImages.length)]})`;
 });
+
+
+
+function playGame(gameUrl) {
+    // Создаем контейнер для игры
+    var gameContainer = document.createElement("div");
+    gameContainer.id = "game-container";
+    gameContainer.style.position = "fixed";
+    gameContainer.style.top = "0";
+    gameContainer.style.left = "0";
+    gameContainer.style.width = "100%";
+    gameContainer.style.height = "100%";
+    gameContainer.style.zIndex = "9999";
+    
+    // Создаем iframe для игры
+    var iframe = document.createElement("iframe");
+    iframe.src = gameUrl;
+    iframe.style.width = "100%";
+    iframe.style.height = "100%";
+    iframe.style.border = "none";
+    
+    // Создаем кнопку закрытия
+    var closeButton = document.createElement("button");
+    closeButton.textContent = "Закрыть";
+    closeButton.style.position = "absolute";
+    closeButton.style.top = "10px";
+    closeButton.style.right = "10px";
+    closeButton.addEventListener("click", closeGame);
+    
+    // Создаем кнопку переключения в полноэкранный режим
+    var fullscreenButton = document.createElement("button");
+    fullscreenButton.textContent = "Полный экран";
+    fullscreenButton.style.position = "absolute";
+    fullscreenButton.style.top = "10px";
+    fullscreenButton.style.left = "10px";
+    fullscreenButton.addEventListener("click", toggleFullscreen);
+    
+    // Добавляем элементы на страницу
+    gameContainer.appendChild(closeButton);
+    gameContainer.appendChild(fullscreenButton);
+    gameContainer.appendChild(iframe);
+    document.body.appendChild(gameContainer);
+  }
+  
+  function closeGame() {
+    var gameContainer = document.getElementById("game-container");
+    gameContainer.parentNode.removeChild(gameContainer);
+  }
+  
+  function toggleFullscreen() {
+    var gameContainer = document.getElementById("game-container");
+    if (gameContainer.requestFullscreen) {
+      gameContainer.requestFullscreen();
+    } else if (gameContainer.webkitRequestFullscreen) { /* Safari */
+      gameContainer.webkitRequestFullscreen();
+    } else if (gameContainer.msRequestFullscreen) { /* IE11 */
+      gameContainer.msRequestFullscreen();
+    }
+  }
+
+
+  const elements = document.querySelectorAll('.game_card_button');
+
+elements.forEach(function(element) {
+  element.addEventListener('click', function() {
+    playGame('Plants vs Zombie Memories.html')
+  });
+});
